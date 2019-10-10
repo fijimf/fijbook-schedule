0143315fef5e220338dab43759fab56da986b21b
Jim Frohnhofer <jfrohnhofer@kabbage.com>
2019-10-09 23:18:27 -0400

    Unigration tests running

--- src/test/scala/com/fijimf/deepfij/schedule/ConsolidatedDbSpec.scala
+++ src/test/scala/com/fijimf/deepfij/schedule/ConsolidatedDbSpec.scala
@@ -5,20 +5,20 @@ import java.time.{LocalDate, LocalDateTime}
 import java.util
 
 import cats.effect.{ContextShift, IO, Resource}
-import com.fijimf.deepfi.schedule.model.{Game, Result}
+import com.fijimf.deepfi.schedule.model._
 import com.spotify.docker.client.DockerClient.ListContainersParam
 import com.spotify.docker.client.messages.{ContainerConfig, ContainerCreation, HostConfig, PortBinding}
 import com.spotify.docker.client.{DefaultDockerClient, DockerClient}
 import doobie.hikari.HikariTransactor
 import doobie.util.{Colors, ExecutionContexts}
 import org.flywaydb.core.Flyway
-import org.scalatest.{BeforeAndAfterAll, FunSuite, Matchers}
+import org.scalatest.{BeforeAndAfterAll, FunSpec, Matchers}
 
 import scala.annotation.tailrec
 import scala.collection.JavaConverters._
 import scala.util.{Failure, Success, Try}
 
-class ConsolidatedDbSpec extends FunSuite with BeforeAndAfterAll with Matchers with doobie.scalatest.IOChecker {
+class ConsolidatedDbSpec extends FunSpec with BeforeAndAfterAll with Matchers with doobie.scalatest.IOChecker {
 
   override val colors: Colors.Ansi.type = doobie.util.Colors.Ansi // just for docs
   implicit val cs: ContextShift[IO] = IO.contextShift(ExecutionContexts.synchronous)
@@ -143,53 +143,182 @@ class ConsolidatedDbSpec extends FunSuite with BeforeAndAfterAll with Matchers w
     }
     }.unsafeRunSync()
 
-  test("Insert game") {
-    check(Game.Dao.insert(Game(0L, 1L, LocalDate.now(), LocalDateTime.now(), 5L, 7L, Some("MCI Center"), None, "20110302")))
+  describe("Doobie typechecking Dao's") {
+    describe("Game.Dao") {
+      it("insert should typecheck") {
+        check(Game.Dao.insert(Game(0L, 1L, LocalDate.now(), LocalDateTime.now(), 5L, 7L, Some("MCI Center"), None, "20110302")))
+      }
+
+      it("list should typecheck") {
+        check(Game.Dao.list())
+      }
+
+      it("find should typecheck") {
+        check(Game.Dao.find(99L))
+      }
+
+      it("findByLoadKey should typecheck") {
+        check(Game.Dao.findByLoadKey("20190923"))
+      }
+
+      it("delete should typecheck") {
+        check(Game.Dao.delete(99L))
+      }
+
+      it("update should typecheck") {
+        check(Game.Dao.update(Game(83L, 1L, LocalDate.now(), LocalDateTime.now(), 5L, 7L, Some("MCI Center"), None, "20190911")))
+      }
+    }
+    describe("Result.Dao") {
+
+      it("insert should typecheck") {
+        check(Result.Dao.insert(Result(0L, 1L, 92, 67, 2)))
+      }
+
+      it("list should typecheck") {
+        check(Result.Dao.list())
+      }
+
+      it("find should typecheck") {
+        check(Result.Dao.find(99L))
+      }
+
+      it("delete should typecheck") {
+        check(Result.Dao.delete(99L))
+      }
+
+      it("deleteByGameId should typecheck") {
+        check(Result.Dao.deleteByGameId(99L))
+      }
+
+      it("update should typecheck") {
+        check(Result.Dao.insert(Result(3L, 1L, 92, 67, 2)))
+      }
+    }
+    describe("Season.Dao") {
+
+      it("insert should typecheck") {
+        check(Season.Dao.insert(Season(0L, 1999)))
+      }
+
+
+      it("list should typecheck") {
+        check(Season.Dao.list())
   }
 
-  test("List games") {
-    check(Game.Dao.list())
+      it("find should typecheck") {
+        check(Season.Dao.find(99L))
+      }
+
+      it("findByDate should typecheck") {
+        check(Season.Dao.findByDate(LocalDateTime.now()))
+      }
+
+      it("delete should typecheck") {
+        check(Season.Dao.delete(99L))
+      }
+
+      it("update should typecheck") {
+        check(Season.Dao.update(Season(3L, 1999)))
+      }
+    }
+    describe("Team.Dao") {
+
+      it("insert should typecheck") {
+        check(Team.Dao.insert(Team(0L, "georgetown", "Georgetown", "Hoyas", "http://xxx.xxx.com/xxx/xxxxx/xxxxxxxx", "#AAFFDD", "blue")))
+      }
+
+
+      it("list should typecheck") {
+        check(Team.Dao.list())
+      }
+
+      it("find should typecheck") {
+        check(Team.Dao.find(99L))
+      }
+      it("findByAlias should typecheck") {
+        check(Team.Dao.findByAlias("southern-cal"))
+      }
+
+      it("findByKey should typecheck") {
+        check(Team.Dao.findByKey("usc"))
+      }
+
+      it("delete should typecheck") {
+        check(Team.Dao.delete(99L))
+      }
+
+      it("update should typecheck") {
+        check(Team.Dao.update(Team(0L, "georgetown", "Georgetown", "Hoyas", "http://xxx.xxx.com/xxx/xxxxx/xxxxxxxx", "#AAFFDD", "blue")))
+      }
+    }
+    describe("Alias.Dao") {
+
+      it("insert should typecheck") {
+        check(Alias.Dao.insert(Alias(0L, 2L, "st. johns")))
+      }
+
+      it("list should typecheck") {
+        check(Alias.Dao.list())
+      }
+
+      it("find should typecheck") {
+        check(Alias.Dao.find(99L))
+      }
+
+      it("delete should typecheck") {
+        check(Alias.Dao.delete(99L))
+      }
+
+      it("update should typecheck") {
+        check(Alias.Dao.update(Alias(3L, 4L, "ssssss")))
+      }
+
+    }
+    describe("ConferenceMapping.Dao") {
+
+      it("insert should typecheck") {
+        check(ConferenceMapping.Dao.insert(ConferenceMapping(0L, 2L, 3L, 4L)))
+      }
+
+      it("list should typecheck") {
+        check(ConferenceMapping.Dao.list())
+      }
+
+      it("find should typecheck") {
+        check(ConferenceMapping.Dao.find(99L))
+      }
+
+      it("delete should typecheck") {
+        check(ConferenceMapping.Dao.delete(99L))
+      }
+
+      it("update should typecheck") {
+        check(ConferenceMapping.Dao.update(ConferenceMapping(1L, 2L, 3L, 4L)))
+      }
+    }
+
+    describe("Conference.Dao") {
+
+      it("insert should typecheck") {
+        check(Conference.Dao.insert(Conference(0L, "big-east", "Big East", "The Big East Conference", None)))
+      }
+
+      it("list should typecheck") {
+        check(Conference.Dao.list())
+      }
+
+      it("find should typecheck") {
+        check(Conference.Dao.find(99L))
+      }
+
+      it("delete should typecheck") {
+        check(Conference.Dao.delete(99L))
+      }
+
+      it("update should typecheck") {
+        check(Conference.Dao.update(Conference(1L, "big-east", "Big East", "The Big East Conference", None)))
+      }
+    }
   }
-
-  test("Find game") {
-    check(Game.Dao.find(99L))
-  }
-
-  test("FindByLoadKey") {
-    check(Game.Dao.findByLoadKey("20190923"))
-  }
-
-  test("Delete Game") {
-    check(Game.Dao.delete(99L))
-  }
-
-  test("Update Game") {
-    check(Game.Dao.update(Game(83L, 1L, LocalDate.now(), LocalDateTime.now(), 5L, 7L, Some("MCI Center"), None, "20190911")))
-  }
-
-  test("Insert Result") {
-    check(Result.Dao.insert(Result(0L, 1L, 92, 67, 2)))
-  }
-
-  test("List Result") {
-    check(Result.Dao.list())
-  }
-
-  test("Find Result") {
-    check(Result.Dao.find(99L))
-  }
-
-  test("Delete Result") {
-    check(Result.Dao.delete(99L))
-  }
-
-  test("DeleteByGameId") {
-    check(Result.Dao.deleteByGameId(99L))
-  }
-
-  test("Update Result") {
-    check(Result.Dao.insert(Result(3L, 1L, 92, 67, 2)))
-  }
-
-
 }

6ac1d816aa3d5b27047ef5fa9cf163b8aadf0dd1
Jim Frohnhofer <jfrohnhofer@kabbage.com>
2019-10-10 00:46:59 -0400

    Cleanup returning cols

--- src/main/scala/com/fijimf/deepfi/schedule/model/Conference.scala
+++ src/main/scala/com/fijimf/deepfi/schedule/model/Conference.scala
@@ -1,7 +1,6 @@
 package com.fijimf.deepfi.schedule.model
 
 import doobie.implicits._
-import doobie.util.fragment.Fragment
 import doobie.util.update.Update0
 
 final case class Conference(id: Long, key: String, name: String, longName: String, logoUrl: Option[String]) {
@@ -14,29 +13,21 @@ object Conference {
     val cols: Array[String] = Array("id", "key", "name", "long_name", "logo_url")
     val tableName: String = "conference"
     def insert(c: Conference): Update0 =
-      sql"""
-    INSERT INTO conference(key,name,long_name,logo_url)
-    VALUES (${c.key}, ${c.name}, ${c.longName}, ${c.logoUrl})
-    RETURNING $colString """.update
+      (fr"""INSERT INTO conference(key,name,long_name,logo_url)
+             VALUES (${c.key}, ${c.name}, ${c.longName}, ${c.logoUrl})
+             RETURNING """ ++ colFr).update
 
-    def find(id: Long): doobie.Query0[Conference] = (baseQuery ++
-      fr"""
-       WHERE id = $id
-      """).query[Conference]
+    def update(c: Conference): Update0 =
+      (fr"""UPDATE conference set  key=${c.key}, name=${c.name}, long_name=${c.longName}, logo_url=${c.logoUrl}
+            WHERE id= ${c.id}
+            RETURNING """ ++ colFr).update
+
+    def find(id: Long): doobie.Query0[Conference] = (baseQuery ++ fr" WHERE id = $id").query[Conference]
 
     def list(): doobie.Query0[Conference] = baseQuery.query[Conference]
 
+    def delete(id: Long): doobie.Update0 = sql"DELETE FROM conference where id=$id".update
 
-    def delete(id: Long): doobie.Update0 =
-      sql"""
-        DELETE FROM conference where id=${id}
-      """.update
-
-    def update(c: Conference): Update0 =
-      sql"""
-        UPDATE conference set  key=${c.key}, name=${c.name}, long_name=${c.longName}, logo_url=${c.logoUrl}
-        WHERE id= ${c.id}
-         RETURNING $colString """.update
   }
 
 }

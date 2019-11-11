3cc328e2f64072988bf621e9a81698e2544d15e6
Jim Frohnhofer <jfrohnhofer@kabbage.com>
2019-11-11 01:10:03 -0500

    Streamlined update hopefully

--- src/main/scala/com/fijimf/deepfij/schedule/services/Updater.scala
+++ src/main/scala/com/fijimf/deepfij/schedule/services/Updater.scala
@@ -12,9 +12,11 @@ import doobie.util.transactor.Transactor
 import org.slf4j.{Logger, LoggerFactory}
 
 final case class Updater[F[_]](xa: Transactor[F])(implicit F: Sync[F]) {
+  import Game.Dao._
   val log: Logger =LoggerFactory.getLogger(Updater.getClass)
   implicit val eqGame: Eq[Game] = Eq.fromUniversalEquals
   implicit val eqResult: Eq[Result] = Eq.fromUniversalEquals
+
   /** Update games and results is a little subtle
    * 1) A 'loadKey' is the key used by the scraping model to break up the updates.  Typically it is by date, but scraping date is not the same as game date -- Hawaii night games.
    * 2) For a given loadKey we want to
@@ -38,84 +40,82 @@ final case class Updater[F[_]](xa: Transactor[F])(implicit F: Sync[F]) {
     }
   }
 
+
   def doUpdates(updatesWithKeys: Map[GameKey, (Game, Option[Result])], gamesWithKeys: Map[GameKey, (Game, Option[Result])], loadKey: String): F[List[(Game, Option[Result])]] = {
-    val keys: Set[GameKey] = updatesWithKeys.keySet ++ gamesWithKeys.keySet
-    F.pure(log.info(s"For $loadKey ${keys.size} unique game keys were generated"))
-    keys.toList.map(k => {
-      F.pure(log.info(s"Game key: $k"))
-      (gamesWithKeys.get(k), updatesWithKeys.get(k)) match {
-        case (Some((g, Some(r))), Some((h, Some(s)))) =>
-          F.pure(log.debug(s"Found old game, old result, new game, new result. UPDATE GAME & UPDATE RESULT"))
-          val h1: Game = h.copy(id = g.id)
-          val s1: Result = s.copy(id = r.id, gameId = r.gameId)
-          if (g === h1 && r === s1) {
-            F.pure(List.empty[(Game, Option[Result])])
-          } else {
-            update(h1, Some(s1))
-          }
-        case (Some((g, None)), Some((h, Some(s)))) =>
-          F.pure(log.debug(s"Found old game, new game, new result. UPDATE GAME & INSERT RESULT"))
-          update(h.copy(id = g.id), Some(s))
-        case (Some((g, Some(r))), Some((h, None))) =>
-          F.pure(log.debug(s"Found old game, old result, new game. UPDATE GAME & DELETE RESULT"))
-          update(h.copy(id = g.id), Some(r.copy(id = -r.id)))
-        case (Some((g, None)), Some((h, None))) =>
-          F.pure(log.debug(s"Found old game, new game. UPDATE GAME"))
-          val h1: Game = h.copy(id = g.id)
-          if (h1 === g) {
-            F.pure(List.empty[(Game, Option[Result])])
-          } else {
-            update(h1, None)
-          }
-        case (Some((g, or)), None) => // No game or result in update pure delete
-          F.pure(log.debug(s"Found old game, maybe old result. DELETE GAME & MAYBE RESULT"))
-          update(g.copy(id = -g.id), or.map(r => r.copy(id = -r.id)))
+    val updateKeys: Set[GameKey] = updatesWithKeys.keySet
+    val existingKeys: Set[GameKey] = gamesWithKeys.keySet
 
-        case (None, Some((g, or))) => // No existing game or result pure insert
-          F.pure(log.debug(s"Found new game, maybe new result. INSERT GAME & MAYBE RESULT"))
-          update(g, or)
+    val inserts: Set[GameKey] = updateKeys.diff(existingKeys)
+    val deletes: Set[GameKey] = existingKeys.diff(updateKeys)
+    val updates: Set[GameKey] = updateKeys.intersect(existingKeys)
 
-        case (None, None) => //cant happen
-          F.pure(List.empty[(Game, Option[Result])])
-      }
-    }).sequence.map(_.flatten)
-  }
-
-  def update(g: Game, or: Option[Result]): F[List[(Game, Option[Result])]] = {
     for {
-      game <- updateGame(g)
-      optionResult <- updateOptionResult(or.map(_.copy(gameId = game.id)))
+      is <- inserts.flatMap(k => updatesWithKeys.get(k)).map((insert _).tupled).toList.sequence
+      ds <- deletes.flatMap(k => gamesWithKeys.get(k)).map((delete _).tupled).toList.sequence
+      us <- (for {
+        k <- updates
+        n <- updatesWithKeys.get(k)
+        o <- gamesWithKeys.get(k) if n.neqv(o)
+      } yield {
+        update(o._1, n._1, o._2, n._2)
+      }).toList.sequence
     } yield {
-      if (game.id < 0) {
-        List.empty[(Game, Option[Result])]
-      } else {
-        List((game, optionResult))
+      is ++ ds ++ us
+    }
+  }
+
+  def insert(game: Game, res: Option[Result]): F[(Game, Option[Result])] = {
+    for {
+      g: Game <- Game.Dao.insert(game).withUniqueGeneratedKeys[Game](Game.Dao.cols: _ *).transact(xa)
+      o: Option[Result] <- res match {
+        case Some(r) =>
+          Result.Dao.insert(r.copy(gameId = g.id))
+            .withUniqueGeneratedKeys[Result](Result.Dao.cols: _*)
+            .transact(xa)
+            .map(Option(_))
+        case None =>
+          F.pure(Option.empty[Result])
       }
+    } yield {
+      (g, o)
     }
   }
 
-  def updateGame(g: Game): F[Game] = {
-    import Game.Dao._
-    if (g.id < 0) {
-      Game.Dao.delete(-g.id).run.transact(xa).map(_ => g)
-    } else if (g.id === 0) {
-      Game.Dao.insert(g).withUniqueGeneratedKeys[Game](Game.Dao.cols: _*).transact(xa)
-    } else {
-      Game.Dao.update(g).withUniqueGeneratedKeys[Game](Game.Dao.cols: _*).transact(xa)
+  def delete(game: Game, res: Option[Result]): F[(Game, Option[Result])] = {
+    for {
+      _ <- Result.Dao.deleteByGameId(game.id).run.transact(xa)
+      _ <- Game.Dao.delete(game.id).run.transact(xa)
+    } yield {
+      (game, res)
+      }
+  }
+
+  def update(g: Game, h: Game, o: Option[Result], p: Option[Result]): F[(Game, Option[Result])] = {
+    for {
+      gg <- gameUpdate(g, h.copy(id = g.id))
+      oo <- optResultUpdate(gg.id, o, p)
+    } yield {
+      (gg, oo)
     }
   }
 
-  def updateOptionResult(optionResult: Option[Result]): F[Option[Result]] = {
-    optionResult match {
-      case Some(result) =>
-        if (result.id < 0) {
-          Result.Dao.delete(-result.id).run.transact(xa).map(_ => None)
-        } else if (result.id === 0) {
-          Result.Dao.insert(result).withUniqueGeneratedKeys[Result](Result.Dao.cols: _*).transact(xa).map(Option(_))
-        } else {
-          Result.Dao.update(result).withUniqueGeneratedKeys[Result](Result.Dao.cols: _*).transact(xa).map(Option(_))
-        }
-      case None => F.pure(None)
+  private def gameUpdate(g: Game, h1: Game): F[Game] = {
+    for {
+      h2 <- Game.Dao.update(h1).withUniqueGeneratedKeys[Game](Game.Dao.cols: _ *).transact(xa)
+    } yield {
+      h2
+    }
+  }
+
+  private def optResultUpdate(gameId:Long, o: Option[Result], p: Option[Result]): F[Option[Result]] = {
+    (o, p) match {
+      case (Some(o1), Some(p1)) =>
+        Result.Dao.update(p1.copy(id = o1.id)).withUniqueGeneratedKeys[Result](Result.Dao.cols: _ *).transact(xa).map(Some(_))
+      case (Some(o1), None) =>
+        Result.Dao.delete(o1.id).run.transact(xa).map(_ => Option.empty[Result])
+      case (None, Some(p1)) =>
+        Result.Dao.insert(p1.copy(gameId=gameId)).withUniqueGeneratedKeys[Result](Result.Dao.cols: _ *).transact(xa).map(Some(_))
+      case (None, None) => F.pure(None)
     }
   }
 
